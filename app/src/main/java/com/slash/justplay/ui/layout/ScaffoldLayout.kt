0a7e9b9b4e8bd922b63cbbdded8e561d455d57eb
Slash <Slash.R.Wei@benQ.com>
2021-10-25 15:29:42 +0800

    初始版本

--- app/src/main/java/com/slash/justplay/ui/layout/ScaffoldLayout.kt
+++ app/src/main/java/com/slash/justplay/ui/layout/ScaffoldLayout.kt
@@ -1,20 +1,9 @@
 package com.slash.justplay.ui.layout
 
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.slash.justplay.R
-import kotlinx.coroutines.launch
+import com.slash.justplay.ui.viewgroup.topAppBarWithBack
 
 /**
  * @author Slash.R.Wei
@@ -23,51 +12,9 @@ import kotlinx.coroutines.launch
 @Composable
 fun CommonScaffold(scaffoldState: ScaffoldState = rememberScaffoldState()) {
     val composableScope = rememberCoroutineScope()
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {},
-                navigationIcon = {
-                    IconButton(onClick = {
-
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "This is a back icon."
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
-                        composableScope.launch {
-                            scaffoldState.drawerState.open()
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Filled.MoreVert,
-                            contentDescription = "This is a menu icon."
-                        )
-                    }
-                }
-            )
-        },
-        drawerContent = {
-            Surface(color = MaterialTheme.colors.primary) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(160.dp)
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.menu),
-                        style = MaterialTheme.typography.h5,
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
-        },
-        scaffoldState = scaffoldState, backgroundColor = Color.LightGray.copy(alpha = 0.3f)
-    ) {
-
-    }
+    topAppBarWithBack(
+        name = "test",
+        scaffoldState = scaffoldState,
+        composableScope = composableScope
+    )
 }
